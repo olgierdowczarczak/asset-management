93efc9563452614d8704ff95aa0e3ef581f6db2c
Olgierd Owczarczak <owczarczakdev@gmail.com>
2025-09-01 21:13:27 +0200

    add pages

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,20 @@
+import { getApi } from '../lib/api.ts';
+
+export type Credentials = {
+    username: string;
+    password: string;
+};
+
+export async function register(credentials: Credentials) {
+    const res = await getApi().post('/api/auth/register', credentials);
+    return res.data['token'];
+}
+
+export async function login(credentials: Credentials) {
+    const res = await getApi().post('/api/auth/login', credentials);
+    return res.data['token'];
+}
+
+export async function logout() {
+    await getApi().post('/api/auth/logout');
+}
