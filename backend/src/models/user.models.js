4d97a7e849b206ec8daef9ff6e78a6c499c422e5
Olgierd Owczarczak <owczarczakdev@gmail.com>
2025-09-02 20:35:12 +0200

    OO-0002

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     id: { type: Number, required: true },
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String },
+    is_admin: { type: Boolean },
+    is_deleted: { type: Boolean, default: false }
 }, { versionKey: false });
 
-userSchema.methods.checkPassword = async function (password) { return bcrypt.compare(password, this.password); };
-userSchema.pre('save', async function (next) {
+UserSchema.methods.checkPassword = async function (password) { return bcrypt.compare(password, this.password); };
+UserSchema.pre('save', async function (next) {
     if (!this.isModified("password")) return next();
     try {
         this.password = await bcrypt.hash(this.password, Number(process.env.JWT_SALT));
@@ -19,4 +21,4 @@ userSchema.pre('save', async function (next) {
     }
 });
 
-export default mongoose.model('users', userSchema);
+export default mongoose.model('users', UserSchema);
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,5 +1,22 @@
 import { Router } from 'express';
+import { 
+    getUser, updateUser, deleteUser,
+    getActiveUsers, getAllUsers, getDeletedUsers,
+    createUser 
+} from '../controllers/user.controllers.js';
+import adminMiddleware from '../middleware/admin.middleware.js';
 
 const router = Router();
+router.route('/')
+    .get(getActiveUsers)
+    .post(adminMiddleware, createUser);
+    
+router.get('/all', adminMiddleware, getAllUsers);
+router.get('/deleted', adminMiddleware, getDeletedUsers);
+router.route('/:_id')
+    .get(getUser)
+    .put(adminMiddleware, updateUser)
+    .patch(adminMiddleware, updateUser)
+    .delete(adminMiddleware, deleteUser);
 
 export default router;
