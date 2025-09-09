0c209c47e1b0b64b596b9f13aecf8c2f9a3eafda
Olgierd Owczarczak <owczarczakdev@gmail.com>
2025-09-09 19:29:48 +0200

    OO-0008

---
+++ backend/src/models/license.models.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const LicenseSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    quantity: { type: Number, required: true }
+}, { versionKey: false });
+
+export default mongoose.model('licenses', LicenseSchema);
